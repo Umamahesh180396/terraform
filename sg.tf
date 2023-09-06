563c260cc61a2480aeca03ce425220c8a42ea2a7
mahesh <maheshthota2136@gmail.com>
2023-09-06 09:50:26 +0530

    Terraform files

--- sg.tf
+++ sg.tf
@@ -4,7 +4,7 @@ resource "aws_security_group" "my_sg" {
 
     ingress {
         from_port = 0
-        to_port = 0
+        to_port = 65535
         protocol = "tcp"
         cidr_blocks = ["0.0.0.0/0"]
     }
